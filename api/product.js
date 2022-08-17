d7c8b82eb55acf835c0abd60a3757b9a9b3d93ae
Diego Zuccoli <frehley3000@gmail.com>
2022-08-17 10:11:48 -0300

    Add files via upload
    otra

--- api/product.js
+++ api/product.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
 router.get("/", async (req, res) => {
     try {
         res.json({
             status: 200,
-            message: "get data seucces",
+            message: "Get data has successfully",
         });
     } catch (error) {
         console.error(error);
-        return res.status(500).send("server error");
+        return res.status(500).send("Server error");
     }
 });
 
