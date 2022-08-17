9fc795c565a336050c9da788ff18f9f3ac7a7b07
Diego Zuccoli <frehley3000@gmail.com>
2022-08-17 10:24:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const data = require("./api/resume");
+const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/resume", data);
+app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${ PORT }`));
